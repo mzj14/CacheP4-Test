c104479d428f42d7768617fc49ea928f4007e00d
mzj14 <mzj14@mails.tsinghua.edu.cn>
2017-05-06 23:22:40 +0800

    add origin and cache version, needs to be carefully tested

---
+++ examples/feature_chain/origin/compile.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+p4c-bmv2 feature_chain.p4 --json config.json
--- examples/feature_chain/run.sh
+++
@@ -1,4 +0,0 @@
-!# /bin/bash
-
-simple_switch -i 1@veth1 -i 2@veth2 -i 3@veth3 -i 4@veth4 -i 5@veth5 -i 6@veth6 -i 7@veth7 -i 8@veth8 -i 9@veth9 -i 10@veth10 --log-console --thrift-port 9091 config.json
-# --log-console
